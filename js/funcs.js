4fdda78b6d831caf5e1b96302fe418c035773b32
Nathan Gurrin-Smith <nathan.gurrinsmith@outlook.com>
2023-03-25 20:50:32 -0400

    feat: added gruvbox theme to personal site; updated links between index and startpage

--- js/funcs.js
+++ js/funcs.js
@@ -44,6 +44,8 @@ function loadTheme(path) {
         cssInputLoc.href = path + "css/future.css";
     } else if (theme === "terminal") {
         cssInputLoc.href = path + "css/terminal.css";
+    } else if (theme == "gruvbox") {
+        cssInputLoc.href = path + "css/gruvbox.css";
     } else {
         var newLink = deleteTheme(location.href);
         cssInputLoc.href = path + "css/classic.css";
